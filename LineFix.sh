fc82e8c5844b1202a5c57f4eda622400c343ae1b
mrdrivingduck <562655624@qq.com>
2020-04-08 16:41:25 +0800

    Refactor for Chart 1-26

--- LineFix.sh
+++ LineFix.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 # The path of containing Defects4J bugs.
-d4jData=/work/users/kliu/Defects4JData/
+d4jData=/home/mrdrivingduck/d4jData/
 
 # The path of Defects4J git repository.
-d4jPath=/work/users/kliu/SearchAPR/defects4j/
+d4jPath=/home/mrdrivingduck/defects4j/
 
 # The fault localization metric used to calculate suspiciousness value of each code line.
 metric=Ochiai
 
 # The buggy project ID: e.g., Chart_1
-bugId=$1
+# bugId=$1
+# java -Xmx4g -cp "target/dependency/*" edu.lu.uni.serval.main.Main_Pos $d4jData $d4jPath $bugId $metric L
 
 
-java -Xmx4g -cp "target/dependency/*" edu.lu.uni.serval.main.Main_Pos $d4jData $d4jPath $bugId $metric L
+proj=Chart
+for bug in $(seq 1 26)
+do
+    java -Xmx4g -cp "target/dependency/*" edu.lu.uni.serval.main.Main_Pos $d4jData $d4jPath ${proj}_${bug} $metric L
+done
--- src/main/java/edu/lu/uni/serval/config/Configuration.java
+++ src/main/java/edu/lu/uni/serval/config/Configuration.java
@@ -5,12 +5,12 @@ public class Configuration {
 	/*
 	 * Data path of Defects4J bugs.
 	 */
-	public static final String BUGGY_PROJECTS_PATH = "/Users/kui.liu/Public/Defects4JData/";
+	public static final String BUGGY_PROJECTS_PATH = "/home/mrdrivingduck/d4jData/";
 	
 	public static final String TEMP_FILES_PATH = ".temp/";
 	public static final long SHELL_RUN_TIMEOUT = 10800L;
 	
-	public static String knownBugPositions = "BugPositions.txt";
+	public static String knownBugPositions = "BugPosotion/GzoltarBugPosition.txt";
 	public static String suspPositionsFilePath = "SuspiciousCodePositions/";
 	public static String failedTestCasesFilePath = "FailedTestCases/";
 	public static String faultLocalizationMetric = "Ochiai";
