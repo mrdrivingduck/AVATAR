c5090f9000c5bc4d477e2bc0b1834a9fa6ce5c42
mrdrivingduck <562655624@qq.com>
2020-05-09 19:22:46 +0800

    Add more bugs in LineFix.sh

--- LineFix.sh
+++ LineFix.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # The path of containing Defects4J bugs.
-d4jData=/home/mrdrivingduck/d4jData/
+d4jData=/home/mrdrivingduck/defects4j/projects/b/
 
 # The path of Defects4J git repository.
 d4jPath=/home/mrdrivingduck/defects4j/
@@ -17,5 +17,118 @@ metric=Ochiai
 proj=Chart
 for bug in $(seq 1 26)
 do
-    java -Xmx4g -cp "target/dependency/*" edu.lu.uni.serval.main.Main_Pos $d4jData $d4jPath ${proj}_${bug} $metric L
+    # java -Xmx4g -cp "target/dependency/*" edu.lu.uni.serval.main.Main_Pos $d4jData $d4jPath ${proj}_${bug} $metric L
+    echo ${proj}_${bug}
+done
+
+proj=Cli
+for bug in $(seq 1 5; seq 7 40)
+do
+    # java -Xmx4g -cp "target/dependency/*" edu.lu.uni.serval.main.Main_Pos $d4jData $d4jPath ${proj}_${bug} $metric L
+    echo ${proj}_${bug}
+done
+
+proj=Closure
+for bug in $(seq 1 62; seq 64 92; seq 94 176)
+do
+    # java -Xmx4g -cp "target/dependency/*" edu.lu.uni.serval.main.Main_Pos $d4jData $d4jPath ${proj}_${bug} $metric L
+    echo ${proj}_${bug}
+done
+
+proj=Codec
+for bug in $(seq 1 18)
+do
+    # java -Xmx4g -cp "target/dependency/*" edu.lu.uni.serval.main.Main_Pos $d4jData $d4jPath ${proj}_${bug} $metric L
+    echo ${proj}_${bug}
+done
+
+proj=Collections
+for bug in $(seq 25 28)
+do
+    # java -Xmx4g -cp "target/dependency/*" edu.lu.uni.serval.main.Main_Pos $d4jData $d4jPath ${proj}_${bug} $metric L
+    echo ${proj}_${bug}
+done
+
+proj=Compress
+for bug in $(seq 1 47)
+do
+    # java -Xmx4g -cp "target/dependency/*" edu.lu.uni.serval.main.Main_Pos $d4jData $d4jPath ${proj}_${bug} $metric L
+    echo ${proj}_${bug}
+done
+
+proj=Csv
+for bug in $(seq 1 16)
+do
+    # java -Xmx4g -cp "target/dependency/*" edu.lu.uni.serval.main.Main_Pos $d4jData $d4jPath ${proj}_${bug} $metric L
+    echo ${proj}_${bug}
+done
+
+proj=Gson
+for bug in $(seq 1 18)
+do
+    # java -Xmx4g -cp "target/dependency/*" edu.lu.uni.serval.main.Main_Pos $d4jData $d4jPath ${proj}_${bug} $metric L
+    echo ${proj}_${bug}
+done
+
+proj=JacksonCore
+for bug in $(seq 1 26)
+do
+    # java -Xmx4g -cp "target/dependency/*" edu.lu.uni.serval.main.Main_Pos $d4jData $d4jPath ${proj}_${bug} $metric L
+    echo ${proj}_${bug}
+done
+
+proj=JacksonDatabind
+for bug in $(seq 1 112)
+do
+    # java -Xmx4g -cp "target/dependency/*" edu.lu.uni.serval.main.Main_Pos $d4jData $d4jPath ${proj}_${bug} $metric L
+    echo ${proj}_${bug}
+done
+
+proj=JacksonXml
+for bug in $(seq 1 6)
+do
+    # java -Xmx4g -cp "target/dependency/*" edu.lu.uni.serval.main.Main_Pos $d4jData $d4jPath ${proj}_${bug} $metric L
+    echo ${proj}_${bug}
+done
+
+proj=Jsoup
+for bug in $(seq 1 93)
+do
+    # java -Xmx4g -cp "target/dependency/*" edu.lu.uni.serval.main.Main_Pos $d4jData $d4jPath ${proj}_${bug} $metric L
+    echo ${proj}_${bug}
+done
+
+proj=JxPath
+for bug in $(seq 1 22)
+do
+    # java -Xmx4g -cp "target/dependency/*" edu.lu.uni.serval.main.Main_Pos $d4jData $d4jPath ${proj}_${bug} $metric L
+    echo ${proj}_${bug}
+done
+
+proj=Lang
+for bug in $(seq 1 1; seq 3 65)
+do
+    # java -Xmx4g -cp "target/dependency/*" edu.lu.uni.serval.main.Main_Pos $d4jData $d4jPath ${proj}_${bug} $metric L
+    echo ${proj}_${bug}
+done
+
+proj=Math
+for bug in $(seq 1 106)
+do
+    # java -Xmx4g -cp "target/dependency/*" edu.lu.uni.serval.main.Main_Pos $d4jData $d4jPath ${proj}_${bug} $metric L
+    echo ${proj}_${bug}
+done
+
+proj=Mockito
+for bug in $(seq 1 38)
+do
+    # java -Xmx4g -cp "target/dependency/*" edu.lu.uni.serval.main.Main_Pos $d4jData $d4jPath ${proj}_${bug} $metric L
+    echo ${proj}_${bug}
+done
+
+proj=Time
+for bug in $(seq 1 20; seq 22 27)
+do
+    # java -Xmx4g -cp "target/dependency/*" edu.lu.uni.serval.main.Main_Pos $d4jData $d4jPath ${proj}_${bug} $metric L
+    echo ${proj}_${bug}
 done
