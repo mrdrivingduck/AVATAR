0bafc0e8e5222f49af3500623fdaa7aee63133f3
mrdrivingduck <562655624@qq.com>
2020-05-13 15:03:51 +0800

    Fixed Chart and Cli

--- src/main/java/edu/lu/uni/serval/config/Configuration.java
+++ src/main/java/edu/lu/uni/serval/config/Configuration.java
@@ -5,12 +5,12 @@ public class Configuration {
 	/*
 	 * Data path of Defects4J bugs.
 	 */
-	public static final String BUGGY_PROJECTS_PATH = "/home/mrdrivingduck/d4jData/";
+	public static final String BUGGY_PROJECTS_PATH = "/home/mrdrivingduck/Desktop/d4jData/";
 	
 	public static final String TEMP_FILES_PATH = ".temp/";
 	public static final long SHELL_RUN_TIMEOUT = 10800L;
 	
-	public static String knownBugPositions = "BugPosition/GZoltarBugPosition.txt";
+	public static String knownBugPositions = "BugPosition/D4jPatchPosition.txt";
 	public static String suspPositionsFilePath = "SuspiciousCodePositions/";
 	public static String failedTestCasesFilePath = "FailedTestCases/";
 	public static String faultLocalizationMetric = "Ochiai";
